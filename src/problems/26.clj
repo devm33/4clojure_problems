fd1701b856f94da093c2c5570dd989559a5b5598
Devraj Mehta <devm33@gmail.com>
2015-04-25 02:52:23 -0400

    fixed namespace

--- src/problems/26.clj
+++ src/problems/26.clj
@@ -1,8 +1,8 @@
-(ns problems.core)
+(ns problems.26)
 
 (defn fib [n]
   (if (< n 2)
     (repeat n 1)
     (let
       [prev (fib (dec n))]
-      (conj '((+ (first prev) (second prev))) (reverse prev)))))
+      (conj (+ (first prev) (second prev)) (reverse prev)))))
