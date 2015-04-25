5755bc0fb357fc8b3da434cb53482b461f2c3174
Devraj Mehta <devm33@gmail.com>
2015-04-25 02:46:36 -0400

    changed to leiningren project

--- 26.clj
+++
@@ -1,9 +0,0 @@
-(fn fib [n]
-  (if (< n 2)
-    (repeat n 1)
-    (let
-      [prev (fib (dec n))]
-      (conj '((+ (first prev) (second prev))) (reverse prev))
-      )
-    )
-  )
---
+++ src/problems/26.clj
@@ -0,0 +1,8 @@
+(ns problems.core)
+
+(defn fib [n]
+  (if (< n 2)
+    (repeat n 1)
+    (let
+      [prev (fib (dec n))]
+      (conj '((+ (first prev) (second prev))) (reverse prev)))))
