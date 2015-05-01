370f9aa2f7b70c6d246b2e3134882da7fe7e5801
Mehta, Devraj <Devraj.Mehta@gtri.gatech.edu>
2015-05-01 17:47:05 -0400

    not working, turning into nonsense

---
+++ src/problems/73.clj
@@ -0,0 +1,44 @@
+(ns problems.73)
+
+(defn __ [rows]
+  (let
+    [checkxo (fn [pred] (if (pred :x) :x (if (pred :o) :o nil)))
+     alleq (fn [c coll] (apply = c coll))
+     hasgroupeq (fn [c groups] (some (partial alleq c) groups))
+     cols (apply map vector rows)]
+    (if-let [res (checkxo #((or (hasgroupeq % rows) (hasgroupeq % cols))))]
+      res
+      (let [d (map-indexed #(nth %2 %1) rows)]
+        (if-let [resd (checkxo #(alleq % d))]
+          resd
+          (let [bd (map-indexed #(nth %2 (- (dec (count (first rows))) %1)) rows)]
+            (checkxo #(alleq % bd))))))))
+
+
+(assert (= nil (__ [[:e :e :e]
+                    [:e :e :e]
+                    [:e :e :e]])))
+
+(assert (= :x (__ [[:x :e :o]
+                   [:x :e :e]
+                   [:x :e :o]])))
+
+(assert (= :o (__ [[:e :x :e]
+                   [:o :o :o]
+                   [:x :e :x]])))
+
+(assert (= nil (__ [[:x :e :o]
+                    [:x :x :e]
+                    [:o :x :o]])))
+
+(assert (= :x (__ [[:x :e :e]
+                   [:o :x :e]
+                   [:o :e :x]])))
+
+(assert (= :o (__ [[:x :e :o]
+                   [:x :o :e]
+                   [:o :e :x]])))
+
+(assert (= nil (__ [[:x :o :x]
+                    [:x :o :x]
+                    [:o :x :o]])))
