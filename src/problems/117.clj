c3ea3d4f53d3de30ad46d68a1bbcaf67a7f9dbfe
Devraj Mehta <devm33@gmail.com>
2015-05-10 03:02:51 -0400

    solved 117

--- src/problems/117.clj
+++ src/problems/117.clj
@@ -4,27 +4,7 @@
 
 ; maze problem
 
-; simple bfs soln
-
-(defn __ [maze]
-  (letfn [(get_maze_at [[r c]]
-            (when (and (< -1 r (count maze)) (< -1 c (count (nth maze r))))
-              (nth (nth maze r) c)))]
-    (loop [visited #{}
-           queue (list (let [r (some #(when (.contains % "M") %) maze)]
-                         (vector (.indexOf maze r) (.indexOf r "M"))))]
-      (if (empty? queue) false
-        (let [cur (last queue)]
-          (if (= \C (get_maze_at cur)) true
-            (recur
-              (conj visited cur)
-              (butlast queue)
-
-              ; TODO add neighbors to queue
-
-                   )))))))
-
-
+; sample maze
 (def test-maze ["#######"
                 "#     #"
                 "#  #  #"
@@ -37,5 +17,72 @@
   (let [r (some #(when (.contains % "M") %) maze)]
     (vector (.indexOf maze r) (.indexOf r "M"))))
 
-(find_m test-maze)
+(assert (= [3 1] (find_m test-maze)))
 
+; simple bfs soln
+
+(defn __ [maze]
+  (letfn [(get_maze_at [[r c]]
+            (when (and (< -1 r (count maze)) (< -1 c (count (nth maze r))))
+              (nth (nth maze r) c)))]
+    (loop [visited #{}
+           queue (list (let [r (some #(when (.contains % "M") %) maze)]
+                         (vector (.indexOf maze r) (.indexOf r "M"))))]
+      (if (empty? queue) false
+        (let [cur (first queue)]
+          (if (= \C (get_maze_at cur)) true
+            (recur
+              (conj visited cur)
+              (concat
+                (rest queue)
+                (loop [d [[1 0] [-1 0] [0 1] [0 -1]] r []]
+                  (if (empty? d) r
+                    (recur
+                      (rest d)
+                      (let [p (map + cur (first d)) v (get_maze_at p)]
+                        (if (and v (not= \# v) (not (contains? visited p)))
+                          (conj r p) r)))))))))))))
+
+(assert (__ test-maze))
+
+(assert (= true  (__ ["M   C"])))
+
+(assert (= false (__ ["M # C"])))
+
+(assert (= true  (__ ["#######"
+              "#     #"
+              "#  #  #"
+              "#M # C#"
+              "#######"])))
+
+(assert (= false (__ ["########"
+              "#M  #  #"
+              "#   #  #"
+              "# # #  #"
+              "#   #  #"
+              "#  #   #"
+              "#  # # #"
+              "#  #   #"
+              "#  #  C#"
+              "########"])))
+
+(assert (= false (__ ["M     "
+              "      "
+              "      "
+              "      "
+              "    ##"
+              "    #C"])))
+
+(assert (= true  (__ ["C######"
+              " #     "
+              " #   # "
+              " #   #M"
+              "     # "])))
+
+(assert (= true  (__ ["C# # # #"
+              "        "
+              "# # # # "
+              "        "
+              " # # # #"
+              "        "
+              "# # # #M"])))
