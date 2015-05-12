b8ae5de9ec0d00f053e71a5b3905b5c7a65cf87c
Devraj Mehta <devm33@gmail.com>
2015-05-12 00:24:07 -0400

    solved 131

--- src/problems/131.clj
+++ src/problems/131.clj
@@ -1,5 +1,20 @@
 (ns problems.131)
 
-(fn [& sets]
-  (letfn [(power [s] (let [sub (power (rest s))] (apply conj sub (map #(conj % (first s)) sub)))))]
-    
+(defn __ [& sets]
+  (letfn [(power [s]
+            (if (empty? s) #{#{}} (let [sub (power (rest s))]
+                (apply conj sub (map #(conj % (first s)) sub)))))]
+    (->> sets
+      (map #(set (map (partial reduce +) (disj (power %) #{}))))
+      (apply clojure.set/intersection)
+      (empty?)
+      (not))))
+
+
+(assert (__ #{-1 1 99}
+            #{-2 2 888}
+            #{-3 3 7777}))
+(assert (= false (__ #{1}
+                     #{2}
+                     #{3}
+                     #{4})))
