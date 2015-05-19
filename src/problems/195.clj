4164d3102b7fe310ecfe3ec32e4412a03c4af26c
Devraj Mehta <devm33@gmail.com>
2015-05-18 23:15:25 -0400

    solved 195

--- src/problems/195.clj
+++ src/problems/195.clj
@@ -2,11 +2,17 @@
 
 (defn __ [n]
   (if (zero? n) #{""}
-    (set (mapcat #(list (str \( % \)) (str % "()") (str "()" %))
-                 (__ (dec n))))))
+    ((fn br [o c v]
+       (if (zero? o)
+         (if (zero? c) #{}
+           #{(apply str (apply conj v (take c (repeat \)))))})
+         (loop [cc c lv v vs #{}]
+           (if (< cc 0) vs
+             (recur
+               (dec cc)
+               (conj lv \))
+               (apply conj vs (br (dec o) (inc cc) (conj lv \())))))))
+     n 0 [])))
+
 
 (assert (= #{"((()))" "()()()" "()(())" "(())()" "(()())"} (__ 3)))
-
-(count (__ 10))
-
-(count (__ 7))
