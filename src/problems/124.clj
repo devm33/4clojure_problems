dc812b3b81195a6f1b71ad3a776fec4c8b4575ef
Devraj Mehta <devm33@gmail.com>
2015-05-27 01:40:57 -0400

    solved 124

--- src/problems/124.clj
+++ src/problems/124.clj
@@ -3,24 +3,34 @@
 (def __
   (let [dirs (for [x (range -1 2) y (range -1 2)
                    :when (not (and (zero? x) (zero? y)))] [x y])]
-    (fn [board piece])
-    ))
+    (fn [board piece]
+      (let
+        [gb (partial get-in board)
+         op (if (= 'w piece) 'b 'w)
+         +d (fn [d] #(map + % d))
+         mv (fn [p]
+              (when (= 'e (gb p))
+                (set
+                  (mapcat
+                    (fn [d]
+                      (let [p+d (+d d)
+                            pcs (take-while
+                                  #(= op (gb %))
+                                  (iterate p+d (p+d p)))]
+                        (when (and (not-empty pcs)
+                                   (= piece (gb (p+d (last pcs)))))
+                          pcs)))
+                    dirs))))]
+        (apply merge
+          (for [r (range (count board))
+                c (range (count (get board r)))]
+            (let [ps (mv [r c])]
+              (when (not-empty ps)
+                {[r c] ps}))))))))
 
 (def tb '[[e e e e]
           [e w b e]
-          [e b w e]
-          [e e e e]])
+          [e w w e]
+          [e b e e]])
 
-(defn opp [piece] (if (= 'w piece) 'b 'w))
-
-(defn move [p d board piece]
-  (letfn [(incd [p] (map + p d))]
-  (when (= 'e (get-in board p))
-    (let [opp-pieces (take-while
-                       #(= (opp piece) (get-in board %))
-                       (iterate incd (incd p)))]
-      (when (= piece (get-in board (incd (last opp-pieces))))
-        opp-pieces)))))
-
-(move [1 1] [1 0] tb 'b)
-
+(__ tb 'b)
