f004719cab4a0518a792da9afeff6d657b96f35f
Devraj Mehta <devm33@gmail.com>
2015-05-31 20:15:04 -0400

    generating solution for square array case

--- src/problems/152.clj
+++ src/problems/152.clj
@@ -12,6 +12,11 @@
             [B D A]
             [C A B]])
 
+(def ft-sq '[[A B C D]
+             [B A D C]
+             [D C B A]
+             [C D A B]])
+
 ; check if a given square is a valid latin square
 (defn latin? [sq]
   (apply = (concat (map set sq) (apply map hash-set sq))))
@@ -19,3 +24,28 @@
 (assert (latin? t-sq))
 (assert (not (latin? f-sq)))
 (assert (not (latin? d-sq)))
+
+
+; return the number of latin subsquares of size n
+(defn nsubsqs [sq n]
+  (if (= n (count sq))
+    (if (latin? sq) 1 0)
+    (->>
+      (for [r (range (- (count sq) n))
+            c (range (- (count sq) n))]
+        (map #(take n %) (take n sq)))
+      (filter latin?)
+      count)))
+
+(assert (= 1 (subsqs ft-sq 4)))
+(assert (= 4 (subsqs ft-sq 2)))
+
+; return a mapping of sizes to counts of latin subsquares
+(defn sqmap [sq]
+  (->>
+    (range 2 (inc (count sq)))
+    (map #(vector % (nsubsqs sq %)))
+    (filter #(-> % second (not= 0)))
+    (into {})))
+
+(sqmap ft-sq)
