beae842ba929a50d5899be4903baa29983ad6849
Mehta, Devraj <Devraj.Mehta@gtri.gatech.edu>
2015-06-03 19:08:14 -0400

    added alternatives to fib 26

--- src/problems/26.clj
+++ src/problems/26.clj
@@ -8,3 +8,19 @@
         (reverse (conj prev (+ (first prev) (second prev)))))))
 
 (fib  5)
+
+(defn fibo [n]
+  (letfn [(fib [a b] (cons a (lazy-seq (fib b (+ a b)))))]
+    (take n (fib 1 1))))
+
+(fibo 7)
+
+(defn fibloop [n]
+  (loop [remain n ret [] a 0 b 1]
+    (if (= 0 remain) ret
+      (recur (dec remain)
+             (conj ret a)
+             b
+             (+ a b)))))
+
+(fibloop 7)
