28d20379da266a78947cf43be92139cd94570455
Devraj Mehta <devm33@gmail.com>
2015-06-07 14:03:38 -0400

    cleaned up dfa solution for blog post

--- src/problems/164.clj
+++ src/problems/164.clj
@@ -1,28 +1,26 @@
 (ns problems.164)
 
-(defn __ ([dfa] (__ dfa [{:state (:start dfa) :path ""}]))
-  ([dfa trails]
-   (when (not-empty trails)
-     (lazy-seq
-       (let [cur (first trails)
-             ntrans (get-in dfa [:transitions (:state cur)])
-             ntrails (if ntrans
-                       (concat
-                         (rest trails)
-                         (map
-                           (fn [[t s]]
-                             (hash-map :state s :path (str (:path cur) t)))
-                           ntrans))
-                       (rest trails))]
-         (if ((:accepts dfa) (:state cur))
-           (cons (:path cur) (__ dfa ntrails))
-           (__ dfa ntrails)))))))
+(defn __ [dfa]
+  (letfn
+    [(map-trans [cur trans]
+       (when trans
+         (map (fn [[t s]] {:state s :path (str (:path cur) t)}) trans)))
+     (enq [trails]
+       (when (not-empty trails)
+         (lazy-seq
+           (let [cur (first trails)
+                 next-trans (get-in dfa [:transitions (:state cur)])
+                 next-trails (concat (rest trails) (map-trans cur next-trans))]
+             (if ((:accepts dfa) (:state cur))
+               (cons (:path cur) (enq next-trails))
+               (enq next-trails))))))]
+    (enq [{:state (:start dfa) :path ""}])))
 
 (assert (= #{"a" "ab" "abc"}
-   (set (__ '{:states #{q0 q1 q2 q3}
-              :alphabet #{a b c}
-              :start q0
-              :accepts #{q1 q2 q3}
-              :transitions {q0 {a q1}
-                            q1 {b q2}
-                            q2 {c q3}}}))))
+           (set (__ '{:states #{q0 q1 q2 q3}
+                      :alphabet #{a b c}
+                      :start q0
+                      :accepts #{q1 q2 q3}
+                      :transitions {q0 {a q1}
+                                    q1 {b q2}
+                                    q2 {c q3}}}))))
